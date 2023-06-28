6238cc5622a5ece5a038f21348b0436145af1306
panggau <davidajha68@gmail.com>
2023-06-28 11:30:13 +0700

    a

---
+++ app/src/main/java/com/panggau181103375/login/MenuActivity.java
@@ -0,0 +1,17 @@
+package com.panggau181103375.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
